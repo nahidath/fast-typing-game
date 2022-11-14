93019aeebaba87e195b97d360749ec61d52e4074
nahidath <36660735+nahidath@users.noreply.github.com>
2022-11-14 11:18:22 -0500

    still workiing on word api

--- ftg/src/Gameplay/Game.js
+++ ftg/src/Gameplay/Game.js
@@ -5,7 +5,10 @@ export default function Game(){
 
     const [answer, setAnswer] = useState("");
     const [cd, setCD] = useState(3);
-    const [finish, setFinish] = useState(false);
+    const [playing, setPlaying] = useState(false);
+    const [timer, setTimer] = useState(30);
+    const [score, setScore] = useState(0);
+    const [realAnswer, setRealAnswer] = useState("");
 
     useEffect(() => {
         if(cd >1){
@@ -19,16 +22,32 @@ export default function Game(){
         // setTimeout(() =>{clearCD();}, 3000);
     },[])
 
-    const countdown = () => {
+    useEffect(() => {
+        if(playing && timer > 0){
+            setTimeout( ()=> setTimer(timer-1), 1000);
+            // updtScore();
+        }
 
+    },[playing, timer])
+
+    const countdown = () => {
         document.getElementById("countdownStart").innerHTML = "GO !!";
         setTimeout(() => {clearCD();}, 1000);
 
 
     }
     const clearCD = () => {
+        setPlaying(true);
         document.getElementById("countdownStart").className = 'hide';
         document.getElementById("game").className = '';
+        document.getElementById("input").focus();
+    }
+
+    const updtScore = () => {
+        if(answer == realAnswer){
+            setScore(score + 1);
+        }
+
     }
 
     return(
@@ -36,12 +55,13 @@ export default function Game(){
         <div id="countdownStart">{cd}</div>
         <div id="game" className="hide">
             <div className="topStats">
-                <span>Timer :</span>
-                <span>Score :</span>
+                <span>Timer : {timer}</span>
+                <span>Score : {score}</span>
             </div>
 
-            <form className="inputAnswer">
-                <input type="text"  onChange={(e)=> setAnswer(e)} />
+            <div id="wordDisplay"></div>
+            <form className="inputAnswer" onSubmit={updtScore}>
+                <input type="text" id="input"  onChange={(e)=> setAnswer(e)} />
             </form>
 
         </div>
