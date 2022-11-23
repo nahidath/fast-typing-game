e70f2f325a0e51cc43083868cd9133bec3aa61c2
nahidath <36660735+nahidath@users.noreply.github.com>
2022-11-23 15:58:34 -0500

    working on fetch

--- ftg/src/Gameplay/Game.js
+++ ftg/src/Gameplay/Game.js
@@ -10,7 +10,7 @@ export default function Game(){
     const [score, setScore] = useState(0);
     const [word, setWord] = useState("");
     const location = useLocation();
-    const [file, setFile] = useState("");
+    const [file, setFile] = useState(null);
     const [inputEmpty, setInputEmpty] = useState(true);
     const [arrayWords, setArrayWords] = useState([]);
     const [isRunning, setIsRunning] = useState(false);
@@ -18,10 +18,11 @@ export default function Game(){
     const [isCorrect, setIsCorrect] = useState(false);
     const [isLoaded, setIsLoaded] = useState(false);
     useEffect(() => {
-        // if(!isLoaded){
-        //     txtToArray();
-        //     setIsLoaded(true);
-        // }
+        if(!isLoaded){
+            // txtToArray().then(r => console.log(r) );
+            txtToArray();
+            setIsLoaded(true);
+        }
         if (!isRunning){
             let i = cd-1;
             const interval = setInterval(()=>{
@@ -78,35 +79,41 @@ export default function Game(){
 
     }
 
-    const txtToArray = () => {
-        if(location.state.lang === "fr") {
-            fetch('../Languages/fr.txt')
-                .then((r) => r.text())
-                .then(text  => {
-                    setFile(text);
-                })
-        }else if(location.state.lang === "es"){
-            fetch('../Languages/es.txt')
-                .then((r) => r.text())
-                .then(text  => {
-                    setFile(text);
-                })
-        }else if(location.state.lang === "de"){
-            fetch('../Languages/de.txt')
-                .then((r) => r.text())
-                .then(text  => {
-                    setFile(text);
-                })
-        }else if(location.state.lang === "en") {
-            fetch('../Languages/en.txt')
-                .then((r) => r.text())
-                .then(text  => {
-                    setFile(text);
-                })
+    const txtToArray = async () => {
+        let response;
+        if (location.state.lang === "fr") {
+            response = await fetch("../public/Languages/fr.txt");
+            // setFile(response.text());
+            console.log(response);
+            // fetch('./Languages/fr.txt')
+            //     .then((r) => r.text())
+            //     .then(text  => {
+            //         console.log(text);
+            //         setFile(text);
+            //     })
         }
+        // else if (location.state.lang === "es") {
+        //     fetch('../Languages/es.txt')
+        //         .then((r) => r.text())
+        //         .then(text => {
+        //             setFile(text);
+        //         })
+        // } else if (location.state.lang === "de") {
+        //     fetch('../Languages/de.txt')
+        //         .then((r) => r.text())
+        //         .then(text => {
+        //             setFile(text);
+        //         })
+        // } else if (location.state.lang === "en") {
+        //     fetch('../Languages/en.txt')
+        //         .then((r) => r.text())
+        //         .then(text => {
+        //             setFile(text);
+        //         })
+        // }
 
-        setArrayWords(file.split(/\r?\n/));
-        console.log(arrayWords);
+        // setArrayWords(file.split(/\r?\n/));
+        // console.log(arrayWords);
     }
 
     const getRandomWord = () => {
